e8558c54aadabda88826bd3c0b02eb9d83bde879
Hamilton <110085367+hamiltonGomes@users.noreply.github.com>
2023-08-27 15:56:19 -0300

    Update Exercise18.java

--- src/exercises/Exercise18.java
+++ src/exercises/Exercise18.java
@@ -2,7 +2,7 @@ package exercises;
 
 import java.util.Scanner;
 
-public class Main {
+public class Exercise18 {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
